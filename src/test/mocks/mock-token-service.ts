81e555d23ed43e209e683134db8b701331442ae9
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-08-06 15:35:11 -0300

    type fix

--- src/test/mocks/mock-token-service.ts
+++ src/test/mocks/mock-token-service.ts
@@ -2,13 +2,13 @@ import { User } from '$lib/entities/user';
 import type { Token_Service, Token_Validate_Res } from '$lib/use-cases/users-logic';
 
 export class Mock_Token_Service implements Token_Service {
-	validate(token: string): Token_Validate_Res {
+	async validate(token: string): Promise<Token_Validate_Res> {
 		if (token.includes('token')) {
 			return { valid: true, user: new User(1, '', '', []) };
 		}
 		return { valid: false };
 	}
-	create_token(user: User): string {
+	async create_token(user: User): Promise<string> {
 		return `token(${user.id})`;
 	}
 	get_max_age_seconds(): number {
