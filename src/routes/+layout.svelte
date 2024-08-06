1cbe01573f68191b536b97d35673458aced18ceb
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-08-06 20:51:23 -0300

    add view transtion

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,20 @@
 	import '../app.css';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { ModeWatcher } from 'mode-watcher';
+	import { onNavigate } from '$app/navigation';
+
+	onNavigate((navigation) => {
+		// @ts-expect-error STILL BEING ADDED TO BROWSERS
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			// @ts-expect-error STILL BEING ADDED TO BROWSERS
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <Toaster />
