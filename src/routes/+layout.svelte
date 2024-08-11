ae5d36870e8acb42c17f6fa8661fa374ac8c22e8
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-08-10 21:20:45 -0300

    commands

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { ModeWatcher } from 'mode-watcher';
-	import { onNavigate } from '$app/navigation';
+
+	import { onMount } from 'svelte';
+	import { goto, onNavigate } from '$app/navigation';
+	import * as Command from '$lib/components/ui/command/index.js';
+	import type { LayoutRouteId } from './$types';
 
 	onNavigate((navigation) => {
 		// @ts-expect-error STILL BEING ADDED TO BROWSERS
@@ -16,6 +20,49 @@
 			});
 		});
 	});
+
+	let open = false;
+
+	onMount(() => {
+		function handleKeydown(e: KeyboardEvent) {
+			if (e.key === 'k' && (e.metaKey || e.ctrlKey)) {
+				e.preventDefault();
+				open = !open;
+			}
+		}
+
+		document.addEventListener('keydown', handleKeydown);
+		return () => {
+			document.removeEventListener('keydown', handleKeydown);
+		};
+	});
+
+	const commands_admin = [
+		{
+			url: '/admin/category',
+			name: 'Categoria'
+		},
+		{
+			url: '/admin/users',
+			name: 'Usuarios'
+		},
+		{
+			url: '/admin/product',
+			name: 'Productos'
+		}
+	] satisfies Array<{
+		url: LayoutRouteId;
+		name: string;
+	}>;
+
+	function navigate(url: string) {
+		open = false;
+		setTimeout(() => {
+			goto(url, {
+				replaceState: true
+			});
+		}, 100);
+	}
 </script>
 
 <Toaster />
@@ -23,3 +70,17 @@
 <ModeWatcher />
 
 <slot></slot>
+
+<Command.Dialog bind:open>
+	<Command.Input placeholder="Type a command or search..." />
+	<Command.List>
+		<Command.Empty>No results found.</Command.Empty>
+		<Command.Group heading="Admin">
+			{#each commands_admin as { url, name }}
+				<Command.Item onSelect={() => navigate(url)}>
+					<span>{name}</span>
+				</Command.Item>
+			{/each}
+		</Command.Group>
+	</Command.List>
+</Command.Dialog>
---
+++ src/routes/admin/category/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<main class="container pt-6">
+	<div class="relative mb-6">
+		<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Admin Category</h1>
+		<div class="absolute right-4 top-1/4 flex justify-end">
+			<Button>Add new Category</Button>
+		</div>
+	</div>
+</main>
