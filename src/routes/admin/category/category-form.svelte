3d0ee323153aef58fb27c06311b229dd1d781eee
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-08-11 18:57:10 -0300

    create category

---
+++ src/routes/admin/category/category-form.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import * as Form from '$lib/components/ui/form';
+	import { Input } from '$lib/components/ui/input';
+	import { createEventDispatcher } from 'svelte';
+	import { type SuperValidated, superForm } from 'sveltekit-superforms';
+	import { zodClient } from 'sveltekit-superforms/adapters';
+	import { create_category_validator, type Create_Category_Dto } from '$lib/entities/category';
+
+	export let data: SuperValidated<Create_Category_Dto>;
+
+	const dispatch = createEventDispatcher();
+	const form = superForm(data, {
+		validators: zodClient(create_category_validator),
+		onUpdate: (res) => {
+			if (res.result.type == 'success') {
+				dispatch('success', {
+					ok: true
+				});
+			}
+		}
+	});
+
+	const { form: formData, enhance } = form;
+</script>
+
+<form method="POST" use:enhance class="flex flex-col gap-4">
+	<Form.Field {form} name="name">
+		<Form.Control let:attrs>
+			<Form.Label class="text-lg">Name</Form.Label>
+			<Input {...attrs} bind:value={$formData.name} class="w-64" />
+		</Form.Control>
+		<Form.FieldErrors />
+	</Form.Field>
+
+	<div class="mt-4 flex w-64 justify-end gap-3">
+		<Form.Button class="w-6/12 ">Submit</Form.Button>
+	</div>
+</form>
