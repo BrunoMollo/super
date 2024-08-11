a901039efd5e068b57b21c0876de08ea2eddcf5f
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-08-11 19:08:09 -0300

    type check

--- src/test/mocks/mock-category-repo.ts
+++ src/test/mocks/mock-category-repo.ts
@@ -1,5 +1,5 @@
 import { Category } from '$lib/entities/category';
-import type { Category_Repo } from '$lib/use-cases/category-logic';
+import type { Category_Repo } from '$lib/use-cases/ports/i-category-repo';
 import { Basic_Mock_Repo } from './basic-mock-repo';
 
 export class Mock_Category_Repo extends Basic_Mock_Repo<Category> implements Category_Repo {
