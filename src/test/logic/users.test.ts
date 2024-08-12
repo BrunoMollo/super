db78cc852a4124674d04aba08e0032076ad8a695
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-08-12 20:32:52 -0300

    add test to User_Controller

--- src/test/logic/users.test.ts
+++ src/test/logic/users.test.ts
@@ -1,8 +1,9 @@
-import { beforeEach, describe, expect, test } from 'vitest';
+import { beforeAll, beforeEach, describe, expect, test } from 'vitest';
+import { login_validator } from '$lib/entities/user';
 import { User, create_user_validator } from '$lib/entities/user';
-import { IntegrityError } from '$lib/errors';
+import { IntegrityError, LoginError } from '$lib/errors';
 import { User_Controller } from '$lib/logic/users-controller';
-import { JWT_Service } from '$lib/services/jwt_service';
+import { Mock_Token_Service } from '../mocks/mock-token-service';
 import { Mock_User_Repo } from '../mocks/mock-user-repo';
 
 let user_ctrl: User_Controller;
@@ -10,7 +11,7 @@ let user_ctrl: User_Controller;
 beforeEach(() => {
 	const data = [new User(1, 'bruno', 'some-psw', [{ id: 1, name: 'ADMIN' }])];
 	const mock = new Mock_User_Repo(data);
-	const token_service = new JWT_Service();
+	const token_service = new Mock_Token_Service();
 	user_ctrl = new User_Controller(mock, token_service);
 });
 
@@ -36,5 +37,63 @@ describe('user creation', () => {
 		expect(res).instanceof(IntegrityError);
 		const all = await user_ctrl.list_all();
 		expect(all.length).toBe(1);
+	});
+});
+
+describe('user login', () => {
+	test('happy path', async () => {
+		//Create
+		const admin = create_user_validator.parse({
+			username: 'admin',
+			password: '1234',
+			roles_id: [1]
+		});
+		await user_ctrl.create(admin);
+
+		//Login
+		const creds = login_validator.parse({
+			username: 'admin',
+			password: '1234'
+		});
+		const res = await user_ctrl.login(creds);
+		expect(res).not.instanceof(LoginError);
+		expect(res).toHaveProperty('token', 'token(1)');
+	});
+	test('reject login (wrong password)', async () => {
+		//Create
+		const admin = create_user_validator.parse({
+			username: 'admin',
+			password: '1234',
+			roles_id: [1]
+		});
+		await user_ctrl.create(admin);
+
+		//Login
+		const creds = login_validator.parse({
+			username: 'admin',
+			password: '4321 (wrong)'
+		});
+		const res = await user_ctrl.login(creds);
+		expect(res).instanceof(LoginError);
+		expect(res).not.toHaveProperty('token');
+	});
+
+	test('reject login (wrong username)', async () => {
+		//Create
+		const admin = create_user_validator.parse({
+			username: 'admin',
+			password: '1234',
+			roles_id: [1]
+		});
+		await user_ctrl.create(admin);
+
+		//Login
+		const creds = login_validator.parse({
+			username: 'someone else',
+			password: '1234'
+		});
+		const res = await user_ctrl.login(creds);
+		expect(res).instanceof(LoginError);
+		expect(res).not.toHaveProperty('token');
 	});
 });
