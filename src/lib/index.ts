d9cfdedafa870fb2cc5b84323df093ab34da2430
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-08-16 19:48:01 -0300

    organize

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
-// place files you want to import through the `$lib` alias in this folder.
 import { dev } from '$app/environment';
+import { db } from './server/drizzle/drizzle-client';
 import { create_category_validator } from './entities/category';
 import { create_user_validator } from './entities/user';
 import { Category_Controller } from './logic/category-controller';
@@ -8,23 +8,23 @@ import { User_Controller } from './logic/users-controller';
 import { Category_Repo_Drizzle } from './repos/category-repo-drizzle';
 import { Role_Repo_Drizzle } from './repos/role-repo-drizzle';
 import { User_Repo_Drizzle } from './repos/user-repo-drizzle';
-import { db } from './server/drizzle/drizzle-client';
 import { Hash_Service_Bcrypt } from './services/hash_service';
 import { JWT_Service } from './services/jwt_service';
 
+// Serivces
 const hash_service = new Hash_Service_Bcrypt();
+const token_service = new JWT_Service();
 
+//Repos
+const role_repo = new Role_Repo_Drizzle(db);
 const user_repo = new User_Repo_Drizzle(db, hash_service);
 const category_repo = new Category_Repo_Drizzle(db);
 
-export const token_service = new JWT_Service();
-
+//Controllers
+export const role_controller = new Role_Controller(role_repo);
 export const user_controller = new User_Controller(user_repo, token_service);
-
 export const category_controller = new Category_Controller(category_repo);
 
-const role_repo = new Role_Repo_Drizzle(db);
-const role_controller = new Role_Controller(role_repo);
 
 // Seed
 if (dev) {
