acb02051bc4f9196e915bb2ceeb4f74c1e948f92
BrunoMollo <BrunoMollo@users.noreply.github.com>
2024-08-19 19:31:09 +0000

    [Github Action]->format

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,8 +5,8 @@ import { Category_Controller } from './logic/category-controller';
 import { Role_Controller } from './logic/role-controller';
 import { User_Controller } from './logic/users-controller';
 import { Category_Repo_Drizzle } from './repos/category-repo-drizzle';
-import { Unit_of_Work_Drizzle } from './repos/unit-of-work';
 import { Role_Repo_Drizzle } from './repos/role-repo-drizzle';
+import { Unit_of_Work_Drizzle } from './repos/unit-of-work';
 import { User_Repo_Drizzle } from './repos/user-repo-drizzle';
 import { db } from './server/drizzle/drizzle-client';
 import { Hash_Service_Bcrypt } from './services/hash_service';
@@ -22,7 +22,7 @@ const user_repo = new User_Repo_Drizzle(db, hash_service);
 const category_repo = new Category_Repo_Drizzle(db);
 
 // Unit of Work
-const uow = new Unit_of_Work_Drizzle(db)
+const uow = new Unit_of_Work_Drizzle(db);
 
 //Controllers
 export const role_controller = new Role_Controller(role_repo);
@@ -31,18 +31,18 @@ export const category_controller = new Category_Controller(category_repo);
 
 // Seed
 if (dev) {
-  await role_controller.create_or_skip({ id: 1, name: 'ADMIN' });
-  await role_controller.create_or_skip({ id: 2, name: 'SELLER' });
+	await role_controller.create_or_skip({ id: 1, name: 'ADMIN' });
+	await role_controller.create_or_skip({ id: 2, name: 'SELLER' });
 
-  const bruno = create_user_validator.parse({
-    username: 'bruno',
-    password: '1234',
-    roles_id: [1]
-  });
-  await user_controller.create(bruno);
+	const bruno = create_user_validator.parse({
+		username: 'bruno',
+		password: '1234',
+		roles_id: [1]
+	});
+	await user_controller.create(bruno);
 
-  const category = create_category_validator.parse({
-    name: 'Lacteos'
-  });
-  await category_controller.create(category);
+	const category = create_category_validator.parse({
+		name: 'Lacteos'
+	});
+	await category_controller.create(category);
 }
--- src/lib/logic/category-controller.ts
+++ src/lib/logic/category-controller.ts
@@ -3,19 +3,17 @@ import { IntegrityError } from '$lib/errors';
 import type { Category_Repo } from './ports/i-category-repo';
 
 export class Category_Controller {
-  constructor(
-    private category_repo: Category_Repo,
-  ) { }
+	constructor(private category_repo: Category_Repo) {}
 
-  async list_all() {
-    return this.category_repo.get_all();
-  }
+	async list_all() {
+		return this.category_repo.get_all();
+	}
 
-  async create(category: Create_Category_Dto) {
-    const match = await this.category_repo.get_by_name(category.name);
-    if (match) {
-      return new IntegrityError(`Duplicated name "${category.name}"`);
-    }
-    return this.category_repo.create(category);
-  }
+	async create(category: Create_Category_Dto) {
+		const match = await this.category_repo.get_by_name(category.name);
+		if (match) {
+			return new IntegrityError(`Duplicated name "${category.name}"`);
+		}
+		return this.category_repo.create(category);
+	}
 }
