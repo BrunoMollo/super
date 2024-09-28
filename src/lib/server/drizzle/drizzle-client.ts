b919464e0c2876be1036cbc9764fa41b34c33d47
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-09-28 19:22:31 -0300

    seeder

--- src/lib/server/drizzle/drizzle-client.ts
+++ src/lib/server/drizzle/drizzle-client.ts
@@ -1,6 +1,5 @@
 import { DATABASE_URI } from '$env/static/private';
 import { drizzle } from 'drizzle-orm/postgres-js';
-import { migrate } from 'drizzle-orm/postgres-js/migrator';
 import postgres from 'postgres';
 import { dev } from '$app/environment';
 
@@ -8,8 +7,3 @@ const pg_client = postgres(DATABASE_URI, { prepare: false });
 
 export const db = drizzle(pg_client, { logger: dev });
 export type DB_Context = typeof db;
-
-if (dev) {
-	console.log('Running migrations (dev)');
-	await migrate(db, { migrationsFolder: 'migrations', migrationsTable: 'my_migrations' });
-}
