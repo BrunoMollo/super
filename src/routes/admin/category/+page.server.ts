66200a3554d2074c37ec2b229a3490aa19633c73
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-09-30 12:31:21 -0300

    auth category controller

--- src/routes/admin/category/+page.server.ts
+++ src/routes/admin/category/+page.server.ts
@@ -4,10 +4,14 @@ import { zod } from 'sveltekit-superforms/adapters';
 import { create_category_validator } from '$lib/entities/category';
 import { exaust } from '$lib/logic/helpers/results';
 import { serilize } from '$lib/utils/parsing';
+import type { LayoutRouteId } from '../../$types';
 import type { Actions, PageServerLoad } from '../users/$types';
+import { redirect } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async () => {
-	const res = await category_controller.list_all();
+export const load: PageServerLoad = async ({ locals }) => {
+	const { user } = locals;
+
+	const res = await category_controller.list_all(user);
 
 	switch (res.status) {
 		case 'ok': {
@@ -16,21 +20,27 @@ export const load: PageServerLoad = async () => {
 				form: await superValidate(zod(create_category_validator))
 			};
 		}
+		case 'unauthorized': {
+			const url = '/login' satisfies LayoutRouteId;
+			return redirect(401, url);
+		}
 		default:
-			exaust(res.status);
+			exaust(res);
 	}
 };
 
 export const actions: Actions = {
-	default: async (event) => {
-		const form = await superValidate(event, zod(create_category_validator));
+	default: async ({ request, locals }) => {
+		const form = await superValidate(request, zod(create_category_validator));
 		if (!form.valid) {
 			return fail(400, {
 				form
 			});
 		}
 
-		const res = await category_controller.create(form.data);
+		const { user } = locals;
+
+		const res = await category_controller.create(form.data, user);
 
 		switch (res.status) {
 			case 'ok': {
@@ -38,6 +48,10 @@ export const actions: Actions = {
 			}
 			case 'duplicated-name': {
 				return setError(form, 'name', 'This name already exists');
+			}
+			case 'unauthorized': {
+				const url = '/login' satisfies LayoutRouteId;
+				return redirect(401, url);
 			}
 			default:
 				exaust(res);
--- src/routes/admin/category/[category_id]/+page.server.ts
+++ src/routes/admin/category/[category_id]/+page.server.ts
@@ -3,13 +3,16 @@ import { zod } from 'sveltekit-superforms/adapters';
 import { setError, superValidate } from 'sveltekit-superforms/client';
 import { edit_category_validator } from '$lib/entities/category';
 import { exaust } from '$lib/logic/helpers/results';
+import type { LayoutRouteId } from '../../../$types';
 import type { Actions, PageServerLoad } from './$types';
-import { error } from '@sveltejs/kit';
+import { error, redirect } from '@sveltejs/kit';
 
-export const load: PageServerLoad = async ({ params }) => {
+export const load: PageServerLoad = async ({ params, locals }) => {
 	const id = Number(params.category_id);
 
-	const res = await category_controller.get_one(id);
+	const { user } = locals;
+
+	const res = await category_controller.get_one(id, user);
 
 	switch (res.status) {
 		case 'ok': {
@@ -19,19 +22,25 @@ export const load: PageServerLoad = async ({ params }) => {
 		case 'not-found': {
 			return error(404, 'not found');
 		}
+		case 'unauthorized': {
+			const url = '/login' satisfies LayoutRouteId;
+			return redirect(401, url);
+		}
 		default:
 			exaust(res);
 	}
 };
 
 export const actions: Actions = {
-	default: async ({ request }) => {
+	default: async ({ request, locals }) => {
 		const form = await superValidate(request, zod(edit_category_validator));
 		if (!form.valid) {
 			return { form };
 		}
 
-		const res = await category_controller.edit(form.data);
+		const { user } = locals;
+
+		const res = await category_controller.edit(form.data, user);
 
 		switch (res.status) {
 			case 'ok': {
@@ -42,6 +51,10 @@ export const actions: Actions = {
 			}
 			case 'duplicated-name': {
 				return setError(form, 'name', 'There is already a category with this name');
+			}
+			case 'unauthorized': {
+				const url = '/login' satisfies LayoutRouteId;
+				return redirect(401, url);
 			}
 			default:
 				exaust(res);
