d6d881d164f7ef148f6066a211e0b44025ee3cb8
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-11-27 22:35:45 -0300

    rm migrations

--- .github/workflows/migration-check.sh
+++
@@ -1,7 +0,0 @@
-!#/usr/bin/bash
-if pnpm add-migration | grep "No schema changes, nothing to migrate"; then
-	echo "Migrations OK"
-else
-	echo "Migrations Missing"
-	exit 1
-fi
--- migrations/0000_thick_rick_jones.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS "category" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"name" text NOT NULL,
-	"created_at" timestamp DEFAULT now() NOT NULL,
-	CONSTRAINT "category_name_unique" UNIQUE("name")
-);
