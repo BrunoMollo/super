5dd94dea8768765259145636a964ad99e2fe3977
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-12-01 16:39:17 -0300

    context menu in category

--- src/routes/admin/category/category-table-actions.svelte
+++ src/routes/admin/category/category-table-actions.svelte
@@ -2,22 +2,28 @@
 	import type { LayoutRouteId } from '../$types';
 
 	import { page } from '$app/stores';
-	import { buttonVariants } from '$lib/components/ui/button/index.js';
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import { pushState } from '$app/navigation';
 	import type { PageData } from './[category_id]/$types';
 	import EditCategoryPage from './[category_id]/+page.svelte';
 	import { shallow_navigate } from '$lib/utils/shallow-routing';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+
+	import Ellipsis from 'lucide-svelte/icons/ellipsis';
+	import { Button } from '$lib/components/ui/button';
 
 	export let id: number;
 	const base_url = '/admin/category' satisfies LayoutRouteId;
 
 	const href = [base_url, id].join('/');
 
-	$: open = $page.state.edit_category_state ? true : false;
+	$: open_dialog = $page.state.edit_category_state ? true : false;
+
+	let open_dropdown = false;
 
 	const goto_category = shallow_navigate<PageData>({
 		on_load: (data) => {
+			open_dropdown = false;
 			const edit_category_state = data;
 			pushState(href, { edit_category_state });
 		},
@@ -28,15 +34,31 @@
 </script>
 
 <Dialog.Root
-	bind:open
+	bind:open={open_dialog}
 	onOpenChange={(open) => {
 		if (!open) {
 			history.back();
 		}
 	}}
 >
-	<a class={buttonVariants({ variant: 'outline' })} {href} on:click={goto_category}>Edit Category</a
-	>
+	<DropdownMenu.Root bind:open={open_dropdown}>
+		<DropdownMenu.Trigger asChild let:builder>
+			<Button variant="ghost" builders={[builder]} size="icon" class="relative h-8 w-8 p-0">
+				<span class="sr-only">Open menu</span>
+				<Ellipsis class="h-4 w-4" />
+			</Button>
+		</DropdownMenu.Trigger>
+		<DropdownMenu.Content>
+			<DropdownMenu.Group>
+				<DropdownMenu.Label>Actions</DropdownMenu.Label>
+			</DropdownMenu.Group>
+			<DropdownMenu.Separator />
+			<DropdownMenu.Item>
+				<a {href} on:click={goto_category}>Edit Category</a>
+			</DropdownMenu.Item>
+			<DropdownMenu.Item on:click={() => false}>Delete Category</DropdownMenu.Item>
+		</DropdownMenu.Content>
+	</DropdownMenu.Root>
 	<Dialog.Content class="sm:max-w-[425px]">
 		{#if $page.state.edit_category_state}
 			<EditCategoryPage data={$page.state.edit_category_state}>
