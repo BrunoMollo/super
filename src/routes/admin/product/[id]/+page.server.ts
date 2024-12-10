b29aee728e9c52566e494610d14bab1375c97d4c
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-12-09 21:24:22 -0300

    Add validation of Product barcode and desc, on create and edit

--- src/routes/admin/product/[id]/+page.server.ts
+++ src/routes/admin/product/[id]/+page.server.ts
@@ -1,5 +1,5 @@
 import { category_repo, product_repo } from '$lib';
-import { fail, superValidate } from 'sveltekit-superforms';
+import { fail, setError, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { update_product_validator } from '../validators';
 import type { PageServerLoad } from './$types';
@@ -44,10 +44,21 @@ export const actions: Actions = {
 			return fail(401, { form });
 		}
 
-		const existing = !!product_repo.get_one(form.data.id);
+		const existing = await product_repo.get_one(form.data.id);
 		if (!existing) {
 			return fail(404, { form });
 		}
+
+		const same_desc = await product_repo.get_by_description(form.data.desc).then((x) => !!x);
+		if (same_desc && existing.desc !== form.data.desc) {
+			return setError(form, 'desc', 'Esta Descripción ya existe');
+		}
+
+		const same_bar_code = await product_repo.get_by_bar_code(form.data.bar_code).then((x) => !!x);
+		if (same_bar_code && existing.bar_code !== form.data.bar_code) {
+			return setError(form, 'bar_code', 'Este código de barras ya existe');
+		}
+
 		await product_repo.update(form.data);
 		return redirect(302, '/admin/product');
 	}
