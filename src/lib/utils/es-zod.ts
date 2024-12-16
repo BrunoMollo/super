9552c769f1b8a74b127e4904ff78cb3927478147
BrunoMollo <BrunoMollo@users.noreply.github.com>
2024-12-16 18:19:01 +0000

    [Github Action]->format

--- src/lib/utils/es-zod.ts
+++ src/lib/utils/es-zod.ts
@@ -1,17 +1,17 @@
-import i18next from "i18next";
-import { z } from "zod";
-import { zodI18nMap } from "zod-i18n-map";
+import i18next from 'i18next';
+import { z } from 'zod';
+import { zodI18nMap } from 'zod-i18n-map';
 // Import your language translation files
-import translation from "zod-i18n-map/locales/es/zod.json";
+import translation from 'zod-i18n-map/locales/es/zod.json';
 
 // lng and resources key depend on your locale.
 i18next.init({
-  lng: "es",
-  resources: {
-    es: { zod: translation },
-  },
+	lng: 'es',
+	resources: {
+		es: { zod: translation }
+	}
 });
 z.setErrorMap(zodI18nMap);
 
 // export configured zod instance
-export { z }
+export { z };
