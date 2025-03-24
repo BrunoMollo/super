446dcfccc59eaed8395bec74d7b763b0a8e031ca
BrunoMollo <dia.mollo.bruno@gmail.com>
2025-03-24 18:43:15 -0300

    generate qr

--- qr-service/index.mjs
+++ qr-service/index.mjs
@@ -4,10 +4,46 @@ import QRCode from 'qrcode';
 const server = fastify({ logger: true });
 
 server.get('/', async (request, reply) => {
-  const text = 'https://example.com'
+
+  const {
+    fecha,
+    cuit,
+    ptoVta,
+    tipoCmp,
+    nroCmp,
+    importe,
+    moneda,
+    ctz,
+    tipoDocRec,
+    nroDocRec,
+    tipoCodAut,
+    codAut,
+  } = request.query
+
+  const json = JSON.stringify({
+    ver: 1,
+    fecha,
+    cuit: Number(cuit),
+    ptoVta: Number(ptoVta),
+    tipoCmp: Number(tipoCmp),
+    nroCmp: Number(nroCmp),
+    importe: Number(importe),
+    moneda,
+    ctz: Number(ctz),
+    tipoDocRec: Number(tipoDocRec),
+    nroDocRec: Number(nroDocRec),
+    tipoCodAut,
+    codAut: Number(codAut),
+  })
+
+
+  const base64 = Buffer.from(json).toString('base64')
+
+
+  const url = 'https://afip.gob.ar/fe/qr/?p=' + base64;
 
   try {
-    const qrCodeBuffer = await QRCode.toBuffer(text, {
+    const qrCodeBuffer = await QRCode.toBuffer(url, {
       errorCorrectionLevel: 'M',
       type: 'png',
       width: 300,
